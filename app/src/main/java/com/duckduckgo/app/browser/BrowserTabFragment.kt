e39ee295d07826abef51b4cf8d70bb7bba7c3182
David González <malmstein@gmail.com>
2025-12-17 15:03:12 +0100

    more tries at keyboard

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -3132,6 +3132,8 @@ class BrowserTabFragment :
     }
 
     private fun showDuckChatBottomSheet() {
+        // InputBottomSheetFragment().show(getParentFragmentManager(), "")
+
         val duckChatContextualSheet = duckChatContextualBottomSheetFactory.create()
         duckChatContextualSheet.show(childFragmentManager, DuckChatContextualBottomSheet.TAG)
     }
